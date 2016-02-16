801aab51ee8de9685773b4485a0cc659809f75e4
amyjoscelyn <amy.joscelyn@flatironschool.com>
2016-02-16 13:11:55 -0500

    initial commit

---
+++ Outside/Outside/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Outside
+//
+//  Created by Amy Joscelyn on 2/16/16.
+//  Copyright © 2016 Amy Joscelyn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Outside/Outside/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Outside
+//
+//  Created by Amy Joscelyn on 2/16/16.
+//  Copyright © 2016 Amy Joscelyn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
