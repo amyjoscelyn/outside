fdc57b75704a0d62f84ad998addfd876a0840609
amyjoscelyn <amy.joscelyn@flatironschool.com>
2016-02-25 15:50:40 -0500

    temperature label displays as red/blue based on whether it feels warmer or cooler than expected, emojis used as stand-ins for weather icons, displayed in giant label on background, logic written for Overcast and Rainy days

--- Outside/Outside/ViewController.m
+++ Outside/Outside/ViewController.m
@@ -13,6 +13,7 @@
 
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet UILabel *currentTempLabel;
+@property (weak, nonatomic) IBOutlet UILabel *iconLabel;
 
 @end
 
@@ -34,7 +35,50 @@
          AMYCurrentForecast *today = dailyForecastModels[0];
          
          NSLog(@"%@: %.2f/%.2f, it's currently %@ (%@)", today.date, today.temperature, today.apparentTemperature, today.weatherSummary, today.weatherIcon);
+         
+         
+         if (today.temperature < today.apparentTemperature)
+         {
+             self.currentTempLabel.textColor = [UIColor redColor];
+         }
+         else if (today.temperature > today.apparentTemperature)
+         {
+             self.currentTempLabel.textColor = [UIColor blueColor];
+         }
+         else
+         {
+             self.currentTempLabel.textColor = [UIColor blackColor];
+         }
+         
          self.currentTempLabel.text = [NSString stringWithFormat:@"%.2f°F", today.apparentTemperature];
+         
+         /*
+          clear day / sunny = 0
+          slightly cloudy = 1
+          mixed cloud and sun = 2
+          mostly cloudy = 3
+          Overcast (summary) = 4
+          sunshowers = 5
+          rain (icon) = 6 [Light rain (rain)]
+          thunderstorm = 7
+          lightning, no rain = 8
+          snowstorm = 9
+          windy = 10
+          gentle breeze = 11
+          foggy = 12
+          clear night = 13
+          */
+         
+         NSArray *weatherIcons = @[ @"☀️" , @"🌤" , @"⛅️" , @"🌥" , @"☁️" , @"🌦" , @"🌧" , @"⛈" , @"🌩" , @"🌨" , @"💨" , @"🌬🌾" , @"🌫" , @"✨🌙" ];
+         
+         if ([today.weatherSummary isEqualToString:@"Overcast"])
+         {
+             self.iconLabel.text = weatherIcons[4];
+         }
+         else if ([today.weatherIcon isEqualToString:@"rain"])
+         {
+             self.iconLabel.text = weatherIcons[6];
+         }
     }];
     
 }
